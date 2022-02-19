3df00ebde54730c164323e7bd6d9553445df24ba
ElectroZhuk <zekai3011@gmail.com>
2022-02-19 21:09:36 +0300

    Первая версия

---
+++ Weapon.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class WeaponProgram
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+
+        public void Fire(Player player)
+        {
+            if (_damage < 0)
+                throw new Exception("Damage is less than 0");
+
+            if (CanFire == false)
+                return
+
+            player.TakeDamage(_damage);
+            _bullets -= 1;
+        }
+
+        private bool CanFire()
+        {
+            return _bullets > 0;
+        }
+    }
+
+    class Player
+    {
+        public int Health { get; private set; }
+
+        public void TakeDamage(int damage)
+        {
+            if (Health <= 0)
+                return
+
+            Health -= Math.Max(Health - damage, 0);
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
+    }
+}
